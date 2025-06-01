9f54ef6597a007600f6656ee10f7cac0b1997e20
Cristian Camilo Vasquez <cvo6372@gmail.com>
2025-06-01 16:50:29 -0500

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div className="text-white p-4">
+      <h1 className="display-4">Dashboard</h1>
+      <p>Bienvenido al panel de control.</p>
+    </div>
+  );
+}
---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Users from "@/components/users/Users";
+
+
+export default function UsersPage() {
+  return (
+    <Users />
+  );
+}
